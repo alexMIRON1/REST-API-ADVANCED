670dc28afb90d081392b91f636d18e601783d825
alex.miron <sashamir99@gmail.com>
2023-01-20 14:23:02 +0200

    CREATE PROJECT

---
+++ src/main/java/com/epam/esm/RestApiAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.epam.esm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/esm/RestApiAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.esm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
